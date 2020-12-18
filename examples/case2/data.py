e04ef504f7d6432360dc0e216338552d2e219aab
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2020-12-18 11:58:02 +0100

    add case 2 - fracture network

---
+++ examples/case2/data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import porepy as pp
+
+# ------------------------------------------------------------------------------#
+
+def test_data():
+    return {"k": perm, "bc": bc, "source": source, "tol": 1e-6}
+
+# ------------------------------------------------------------------------------#
+
+def perm(g, d, flow_solver):
+    # set a fake permeability for the 0d grids
+    if g.dim == 0:
+        return np.zeros(g.num_cells)
+
+    # here is the condition satisfied with a <
+    if g.tags["condition"] == 1:
+        return 1 * np.ones(g.num_cells)
+    # here is the condition satisfied with a >
+    elif g.tags["condition"] == 0:
+        return 10 * np.ones(g.num_cells)
+    else:
+        import pdb; pdb.set_trace()
+        raise ValueError
+
+# ------------------------------------------------------------------------------#
+
+def source(g, d, flow_solver):
+    # set zero source term for the 0d grids
+    if g.dim == 0:
+        return np.zeros(g.num_cells)
+
+    dx = 1
+    cond1 = g.cell_centers[dx, :] <= 0.3
+    cond2 = np.logical_and(g.cell_centers[dx, :] > 0.3,
+                           g.cell_centers[dx, :] < 0.7)
+    cond3 = g.cell_centers[dx, :] >= 0.7
+
+    rhs = g.cell_volumes.copy()
+    rhs[cond1] *= 10
+    rhs[cond2] *= 1
+    rhs[cond3] *= 5
+
+    return rhs
+    #return g.cell_volumes
+
+# ------------------------------------------------------------------------------#
+
+def bc(g, data, tol, flow_solver):
+    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+    b_face_centers = g.face_centers[:, b_faces]
+
+    # define outflow type boundary conditions
+    out_flow = np.logical_or(b_face_centers[0] > 1 - tol,
+                             b_face_centers[1] > 1 - tol)
+
+    # define inflow type boundary conditions
+    in_flow = np.logical_or(b_face_centers[0] < 0 + tol,
+                            b_face_centers[1] < 0 + tol)
+
+    # define the labels and values for the boundary faces
+    labels = np.array(["neu"] * b_faces.size)
+    bc_val = np.zeros(g.num_faces)
+
+    labels[in_flow] = "dir"
+    labels[out_flow] = "dir"
+    bc_val[b_faces[in_flow]] = 0
+    bc_val[b_faces[out_flow]] = 0
+
+    return labels, bc_val
+
+# ------------------------------------------------------------------------------#
---
+++ examples/case2/main.py
@@ -0,0 +1,105 @@
+import numpy as np
+import porepy as pp
+import scipy.sparse as sps
+
+from data import test_data
+
+import sys; sys.path.insert(0, "../../src/")
+from flow import Flow
+from interface import detect_interface
+from exporter import write_network_pvd, make_file_name
+
+# ------------------------------------------------------------------------------#
+
+def condition_interface(flux_threshold, flux, op, tol=0):
+    norm = np.linalg.norm(flux)
+    if op == "<":
+        return (norm - flux_threshold) < tol
+    elif op == ">":
+        return (norm - flux_threshold) > - tol
+    elif op == "==":
+        return (norm - flux_threshold) > - tol and (norm - flux_threshold) < tol
+
+# ------------------------------------------------------------------------------#
+
+def main():
+
+    # tolerance in the computation
+    tol = 1e-10
+
+    # assign the flag for the low permeable fractures
+    mesh_size = 0.5*1e-2
+    tol_network = mesh_size
+    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size / 20}
+
+    # read and mark the original fracture network, the fractures id will be preserved
+    file_name = "network.csv"
+    domain = {"xmin": 0, "xmax": 1, "ymin": -1, "ymax": 1}
+    network = pp.fracture_importer.network_2d_from_csv(file_name, domain=domain)
+    # set the original id
+    network.tags["original_id"] = np.arange(network.num_frac, dtype=np.int)
+    # save the original network
+    network_original = network.copy()
+
+    # set the condition, meaning if for a branch we solve problem with a < (1) or with > (0)
+    # for simplicity we just set all equal
+    network.tags["condition"] = np.ones(network.num_frac, dtype=np.int)
+
+    flux_threshold = 0.15
+    cond = lambda flux, op, tol=0: condition_interface(flux_threshold, flux, op, tol)
+
+    file_name = "case2"
+    folder_name = "./solution/"
+    variable_to_export = [Flow.pressure, Flow.P0_flux, "original_id", "condition"]
+
+    iteration = 0
+    max_iteration = 20
+    okay = False
+    while not okay:
+
+        # INSERIRE LA SORGENTE VETTORIALE PER VEDERE IL CASO DELLA NON-UNICITA'
+
+        print("iteration", iteration)
+
+        # create the grid bucket
+        gb = network.mesh(mesh_kwargs, dfn=True, preserve_fracture_tags=["original_id", "condition"])
+
+        # create the discretization
+        discr = Flow(gb)
+        discr.set_data(test_data())
+
+        # problem solution
+        A, b = discr.matrix_rhs()
+        x = sps.linalg.spsolve(A, b)
+        discr.extract(x)
+
+        # exporter
+        save = pp.Exporter(gb, "sol_" + file_name, folder_name=folder_name)
+        save.write_vtu(variable_to_export, time_step=iteration)
+
+        # save the network points to check if we have reached convergence
+        old_network_pts = network.pts
+
+        # construct the new network such that the interfaces are respected
+        network = detect_interface(gb, network, network_original, discr, cond, tol)
+        # export the current network with the associated tags
+        network_file_name = make_file_name(file_name, iteration)
+        network.to_file(network_file_name, data=network.tags, folder_name=folder_name, binary=False)
+
+        # check if any point in the network has changed
+        all_pts = np.hstack((old_network_pts, network.pts))
+        distances = pp.distances.pointset(all_pts) > tol_network
+        # consider only the block between the old and new points
+        distances = distances[:old_network_pts.shape[1], -network.pts.shape[1]:]
+        # check if an old point has a point equal in the new set
+        check = np.any(np.logical_not(distances), axis=0)
+
+        if np.all(check) or iteration > max_iteration:
+            okay = True
+        iteration += 1
+
+    save.write_pvd(np.arange(iteration), np.arange(iteration))
+    write_network_pvd(file_name, folder_name, np.arange(iteration))
+
+if __name__ == "__main__":
+    main()
